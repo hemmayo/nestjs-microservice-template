cf1f6e64c57ac6de577bac60a2c7ed00076ca313
Emmanuel Popoola <olaayo10@gmail.com>
2020-06-22 18:56:16 +0100

    setup redis

--- services/products/src/main.ts
+++ services/products/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import * as config from 'config';
+
+const logger = new Logger('Main');
+const redisConfig: any = config.get('redis');
+
+// Create the microservice options object
+const microservicesOptions = {
+  transport: Transport.REDIS,
+  options: {
+    url: redisConfig.url,
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microservicesOptions,
+  );
+
+  await app.listen(() => {
+    logger.log(`Products microservice connected to ${redisConfig.url}`);
+  });
 }
 bootstrap();
