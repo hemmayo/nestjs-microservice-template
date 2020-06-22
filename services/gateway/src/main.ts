be91cd00fdacc3c19db8f34552c0a5add0d6dc09
Emmanuel Popoola <olaayo10@gmail.com>
2020-06-22 18:17:40 +0100

    config setup

--- services/gateway/src/main.ts
+++ services/gateway/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const serverConfig: any = config.get('server');
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = serverConfig.port || 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
